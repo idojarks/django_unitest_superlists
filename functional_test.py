28f5b387ae1fab3b98d67be2af964f564971288e
idojarks <idojarks@gmail.com>
2020-04-11 18:59:56 +0900

    first commit

---
+++ functional_test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get('http://localhost:8000')
+
+assert 'Django: the Web framework for perfectionists with deadlines.' in browser.title
