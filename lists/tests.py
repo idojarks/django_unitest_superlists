bef7700f17900823278a032254e8078da9839fbe
idojarks <idojarks@gmail.com>
2020-04-19 11:08:49 +0900

    최소 html을 반환하는 뷰

--- lists/tests.py
+++ lists/tests.py
@@ -2,14 +2,24 @@ from django.test import TestCase
 from django.urls import reverse
 from lists.views import home_page
 import urllib
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_homepage_view(self):
 
         found = reverse('home')
-        print('found : ', found)
+        #print('found : ', found)
 
         found = urllib.parse.quote(reverse('home'))
-        print('found : ', found)
+        #print('found : ', found)
         
         self.assertEqual(found, '/')
+
+    def test_homepage_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+        # response.content는 byte type.
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
