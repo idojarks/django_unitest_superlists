14d3c2ec1c8df13af9d3565ce056deafd5897ea2
idojarks <idojarks@gmail.com>
2020-04-19 18:22:33 +0900

    뷰 함수에 템플릿 사용

--- lists/tests.py
+++ lists/tests.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from lists.views import home_page
 import urllib
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_homepage_view(self):
-
         found = reverse('home')
         #print('found : ', found)
 
@@ -18,8 +18,10 @@ class HomePageTest(TestCase):
     def test_homepage_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        expected_html = render_to_string('home.html')
 
         # response.content는 byte type.
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        #self.assertTrue(response.content.startswith(b'<html>'))
+        #self.assertIn(b'<title>To-Do</title>', response.content)
+        #self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertEqual(expected_html, response.content.decode())
