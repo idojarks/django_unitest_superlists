9491f08e7048d3e20075124bbad1dca0a11bc177
idojarks <idojarks@gmail.com>
2020-04-29 20:53:02 +0900

    FT에 프란시스 스토리를 추가

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -31,6 +31,9 @@ class NewVisitorTest(LiveServerTestCase):
         # 엔터 키를 누르면 페이지가 새로 바뀌고
         # "1: 밥 먹기" 항목이 보인다
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/list/.+')
+        self.check_for_row_in_list_table('1: 밥 먹기')
 
         # 다른 항목을 넣는다
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -42,16 +45,40 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('1: 밥 먹기', [row.text for row in rows])
         self.assertIn('2: 이빨 닦기', [row.text for row in rows])
         
-        # 항목을 더 쓸 수 있는 여분의 텍스트 박스가 있다
-        # 그곳에 "씻기"라고 쓴다
-        self.fail('Finish the test.')
+        # 페이지를 다시 갱신하고, 두 개 아이템이 목록에 보인다.
+        self.check_for_row_in_list_table('2: 이빨 닦기')
+        self.check_for_row_in_list_table('1: 밥 먹기')
 
-        # 페이지가 새로 바뀌고, 두 개의 항목이 보인다
-        # 입력한 내용이 저장되는지 궁금하다
-        # 사이트는 그녀가 쓸 URL을 만들어준다
-        # 그리고 URL의 설명도 함께 제공한다
+        # 새로운 사용자인 프란시스가 사이트에 접속한다.
+        
+        ## 새로운 브라우저 세션을 이용해서 에디스의 정보가
+        ## 쿠키를 통해 유입되는 상황을 방지한다
+        self.browser.quit()
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
-        # 안내 받은 URL로 접속하면 내가 만든 작업 목록을 볼 수 있다
+        # 프란시스가 홈페이지에 접속한다.
+        # 에디스의 리스트는 보이지 않는다
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('밥 먹기', page_text)
+        self.assertNotIn('이빨 닦기', page_text)
 
+        # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
+        # 그는 에디스보다 재미가 없다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('우유 사기')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 프란시스가 전용 URL을 취득한다
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 에디스가 입력한 흔적이 없음을 다시 확인한다
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('밥 먹기', page_text)
+        self.assertNotIn('이빨 닦기', page_text)
+
+        # 둘 다 만족하고 잠자리에 든다
 
 
