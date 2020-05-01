ea708ba85d4004684f3b4e2cb1a5597ca391b636
idojarks <idojarks@gmail.com>
2020-05-01 10:40:29 +0900

    목록 view 추가, list.html 템플릿 추가, 목록 unit test 클래스 추가

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -6,10 +6,15 @@ from django.test import LiveServerTestCase
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())        
-        self.browser.implicitly_wait(3)
+        #self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 새로운 온라인 앱이 나왔다는 소식을 듣고
@@ -19,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 웹 페이지 타이틀과 헤더가 'To-Do'라고 쓰였다
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.assertIn('작업 목록 시작', header_text)
 
         # 작업 하나를 등록한다
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -32,7 +37,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: 밥 먹기" 항목이 보인다
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: 밥 먹기')
 
         # 다른 항목을 넣는다
@@ -71,7 +76,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 프란시스가 전용 URL을 취득한다
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # 에디스가 입력한 흔적이 없음을 다시 확인한다
--- lists/tests.py
+++ lists/tests.py
@@ -26,15 +26,6 @@ class HomePageTest(TestCase):
         
         self.assertEqual(found, '/')
 
-    def test_homepage_returns_correct_html(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = '신규 작업 아이템'
-
-        response = home_page(request)
-
-        self.assertEqual('/', response['location'])
-
     def test_homepage_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -54,7 +45,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
@@ -62,16 +53,6 @@ class HomePageTest(TestCase):
         home_page(request)
 
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
 
 class ItemModelTest(TestCase):
     
@@ -91,3 +72,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
