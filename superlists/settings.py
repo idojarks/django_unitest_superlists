a514e4f700c1f11dc7f41e4304e7e1139c5264c9
idojarks <idojarks@gmail.com>
2020-05-05 17:18:54 +0900

    static 설정

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,3 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 STATIC_ROOT = os.path.join(BASE_DIR, '../lists/static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
