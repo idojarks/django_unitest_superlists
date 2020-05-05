75db769f477bf80fca70efb154c84be754bf2b2c
idojarks <idojarks@gmail.com>
2020-05-05 11:11:43 +0900

    스테이징 테스트용 해킹

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -3,8 +3,25 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Chrome(ChromeDriverManager().install())        
         #self.browser.implicitly_wait(3)
@@ -18,10 +35,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def skip_test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # 새로운 온라인 앱이 나왔다는 소식을 듣고
         # 그 앱을 확인하러 간다
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'라고 쓰였다
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 프란시스가 홈페이지에 접속한다.
         # 에디스의 리스트는 보이지 않는다
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('밥 먹기', page_text)
         self.assertNotIn('이빨 닦기', page_text)
@@ -90,7 +107,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # 에디스는 메인 페이지를 방문한다
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 그녀는 입력 상자가 가운데 배치됐음을 본다
