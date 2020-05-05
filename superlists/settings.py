485cf5b62ea0ad0c777722fe6d77439cb11c531b
idojarks <idojarks@gmail.com>
2020-05-05 17:50:04 +0900

    staticfiles_dirs 수정

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,11 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 #STATIC_ROOT = os.path.join(BASE_DIR, '../lists/static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'lists/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../lists/static'),
-)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
