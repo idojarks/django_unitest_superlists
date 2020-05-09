c3acee44639fe1e038e5bac5379adf79e19a2738
idojarks <idojarks@gmail.com>
2020-05-09 13:49:52 +0900

    FT를 개별 파일로 분할

--- functional_test/test_simple_list_creation.py
+++ functional_test/test_simple_list_creation.py
@@ -1,39 +1,10 @@
+from .base import FunctionalTest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
-#from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import sys
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Chrome(ChromeDriverManager().install())        
-        #self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.refresh()
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+   
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 새로운 온라인 앱이 나왔다는 소식을 듣고
@@ -105,7 +76,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 둘 다 만족하고 잠자리에 든다
 
-    def test_layout_and_styling(self):
+
         # 에디스는 메인 페이지를 방문한다
         self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
@@ -128,3 +99,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=10
         )
 
+
+
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        pass
