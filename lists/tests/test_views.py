b9dc7cae7d69fa97bc4f2e53d15ef34b8cee0779
idojarks <idojarks@gmail.com>
2020-05-16 11:21:08 +0900

    base.html에 form 태그 추가, home_page()에 form 컨텍스트 전달

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,8 +7,12 @@ from django.template.loader import render_to_string
 import re
 from lists.models import Item, List
 from django.utils.html import escape
+from lists.forms import ItemForm
 
 class HomePageTest(TestCase):
+
+    maxDiff = None
+
     @staticmethod
     def remove_csrf(html_code):
         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
@@ -19,13 +23,14 @@ class HomePageTest(TestCase):
             self.remove_csrf(html_code1),
             self.remove_csrf(html_code2)
         )
-    
+   
+    def test_home_page_renders_home_teamplte(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_root_url_resolves_to_homepage_view(self):
-        found = reverse('home')
-        found = urllib.parse.quote(reverse('home'))
-        
-        self.assertEqual(found, '/')
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
     
 class ListViewTest(TestCase):
 
